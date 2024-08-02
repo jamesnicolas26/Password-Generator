f494c6b58936fc57d5919867fb3898bd664a29b3
jamesnicolas26 <58098815+jamesnicolas26@users.noreply.github.com>
2024-08-02 08:31:53 +0800

    Add files via upload

---
+++ App.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+length = int(input("Enter length of password: "))
+print("Generated Password: ", generate_password(length))
